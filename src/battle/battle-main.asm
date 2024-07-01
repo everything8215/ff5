45682ddb944f7099461318b5a64710940297460a
tylerpersonal <tylersafaric99@gmail.com>
2024-07-01 19:40:22 -0400

    Got first 1000 lines to compile, no idea if the byte code is identical

--- src/battle/battle-main.asm
+++ src/battle/battle-main.asm
@@ -16,6 +16,7 @@
 .include "hardware.inc"
 .include "const.inc"
 .include "battle_ram.inc"
+.reloc
 
 .import InitSound_ext, ExecBtlGfx_ext
 .export ExecBattle_ext
@@ -464,3 +465,544 @@ _0207:
         rts
 
 .endproc
+
+; ---------------------------------------------------------------------------
+
+;Wipes structures related to pending animations, message boxes, and damage displays
+.proc WipeDisplayStructures
+
+_0218:
+        ldx #$0090
+WipeAnimBlocks:		;wipes Anim structure and blocking information after it, $3BCC-$3C5B
+        stz ActionAnim::Flags,X
+        dex
+        bpl WipeAnimBlocks
+        txa 		;A now $FF
+        ldx #$037F
+WipeGFXQueueDamage:	;wipes GFXQueue structure and DisplayDamage after it with $FF, $384C-$3BCB
+        sta !GFXQueue,X
+        dex
+        bpl WipeGFXQueueDamage
+        ldx #$005F
+WipeMessagesTimers:	;wipes Message Boxes and Timer structures after them with $FF, $3C5F-$3E8D
+        sta MessageBoxes,X
+        dex
+        bpl WipeMessagesTimers
+        tdc
+        ldx #$000F
+WipeReflectCounters:	;wipes $7B49-7B58
+        stz CounterReflecteeTable,X
+        dex
+        bpl WipeReflectCounters
+        ldx #$002F
+WipeMessageBoxData:	;wipes numbers used for message boxes, $3CBF-$3CEE
+        stz MessageBoxData,X
+        dex
+        bpl WipeMessageBoxData
+        rts
+
+.endproc
+
+; ---------------------------------------------------------------------------
+
+;Recalculate Stats+Level w/Song(X: Character Offset)
+.proc CopyStatsWithBonuses
+
+_0248:
+        phx
+        phy
+        tdc
+        tay
+AddCopyStats:			;adds/copies Str/Agi/Vit/Mag
+        clc
+        lda CharStruct::EquippedStr,X
+        adc CharStruct::BonusStr,X
+        cmp #$64		;100
+        bcc :+
+        lda #$63   		;cap at 99
+:	sta Strength,Y
+        inx
+        iny
+        cpy #$0004
+        bne AddCopyStats
+        ply
+        plx
+        clc
+        lda CharStruct::Level,X
+        adc CharStruct::BonusLevel,X
+        cmp #$64		;100
+        bcc :+
+        lda #$63   		;cap at 99
+:	sta Level
+        rts
+
+.endproc
+
+; ---------------------------------------------------------------------------
+
+
+;Params: 	A = character index
+;		$3C5D (TempDisplayDamage) = displayed damage
+.proc CopyDisplayDamage
+
+_0276:
+        tax
+        lda ROMCombatantReorder,X	;party members after monsters
+        asl
+        tax
+        lda TempDisplayDamage
+        sta DisplayDamage,X
+        lda TempDisplayDamage+1
+        sta DisplayDamage+1,X
+        rts
+
+.endproc
+
+; ---------------------------------------------------------------------------
+
+.proc WipeActionData
+
+_028A:
+        stz wTargetIndex	;**optimize: wastes a byte
+        ldx #$0133
+:	stz $79F9,X	;clears memory $79F9 - $7B2C
+        dex
+        bpl :-
+        txa 		;now $FF
+        ldx #$0010
+:	sta $7B2D,X	;sets memory $7B2D - $7B3D to $FF
+        dex
+        bpl :-
+        tdc
+        rts
+
+.endproc
+
+; ---------------------------------------------------------------------------
+
+;(Random number from 0..99)
+.proc Random_0_99
+
+_02A2:
+        tdc
+        tax
+        lda #$63
+        jmp Random_X_A
+
+.endproc
+
+; ---------------------------------------------------------------------------
+
+;combines MonsterTargets and PartyTargets into a combined target bitmask PPPPMMMM MMMM0000 to match normal index order
+.proc BuildTargetBitmask
+
+_02A9:
+        lda MonsterTargets
+        pha
+        and #$F0
+        lsr
+        lsr
+        lsr
+        lsr
+        ora PartyTargets
+        sta TempTargetBitmask
+        pla
+        and #$0F
+        asl
+        asl
+        asl
+        asl
+        sta TempTargetBitmask+1
+        rts
+
+.endproc
+; ---------------------------------------------------------------------------
+
+;returns with number of targets minus 1 in A and $2620 (TempTargetting) 
+;notably, 0 if single target
+.proc CheckMultiTarget
+
+_02C2:
+        lda MonsterTargets
+        ora PartyTargets
+        jsr CountSetBits
+        dex
+        txa
+        sta TempTargetting
+        rts
+
+.endproc
+
+; ---------------------------------------------------------------------------
+
+.proc RemoveInactiveTargets
+
+_02CF:
+        stz NoValidTargets
+        tdc
+        tax
+        stx $0E
+Loop:
+        lda ActiveParticipants,X
+        beq Inactive
+        ldy $0E
+        lda CharStruct::Status1,Y
+        and #$C0	;dead/stone
+        bne Inactive
+        lda CharStruct::Status4,Y
+        and #$81	;erased/hiding
+        beq Next
+Inactive:
+        phx
+        cpx #$0004	;monster check
+        bcs Monster
+        lda PartyTargets
+        jsr ClearBit_X
+        sta PartyTargets
+        bra NextPLX
+Monster:
+        txa
+        sec
+        sbc #$04	;monster index
+        tax
+        lda MonsterTargets
+        jsr ClearBit_X
+        sta MonsterTargets
+NextPLX:
+        plx
+Next:
+        longa
+        clc
+        lda $0E
+        adc #$0080	;next CharStruct offset
+        sta $0E
+        shorta0
+        inx
+        cpx #$000C	;12 battle participants
+        bne Loop
+        lda PartyTargets
+        ora MonsterTargets
+        bne Ret
+        inc NoValidTargets
+Ret:	rts
+
+.endproc
+
+; ---------------------------------------------------------------------------
+
+;Copies magic data for spell in A from ROM to AttackInfo (offset Y)
+.proc CopyROMMagicInfo
+
+_0324:
+        longa
+        jsr ShiftMultiply::_8	;Size of Magic Data
+        tax
+        shorta0
+        stz $3D
+CopyFirst5:                                                                                                            
+        lda !ROMMagicInfo,X
+        sta !AttackInfo,Y
+        inx
+        iny
+        inc $3D
+        lda $3D
+        cmp #$05
+        bne CopyFirst5
+        iny 			;AttackInfo has 4 bytes that
+        iny 			;don't apply to magic
+        iny
+        iny
+CopyLast3:
+        lda !ROMMagicInfo,X
+        sta !AttackInfo,Y
+        inx
+        iny
+        inc $3D
+        lda $3D
+        cmp #$08
+        bne CopyLast3
+        rts
+
+.endproc
+
+; ---------------------------------------------------------------------------
+
+;Advances the current message box offets to the next set of messgae boxes
+;(for multi-commands like x-magic)
+.proc NextMessageBoxSet
+
+_0356:
+        clc
+        lda MessageBoxOffset
+        adc #$18		;+24, next message box set
+        sta MessageBoxOffset
+        clc
+        lda MessageBoxDataOffset
+        adc #$0C		;+12, next message box data set
+        sta MessageBoxDataOffset
+        rts
+
+.endproc
+
+; ---------------------------------------------------------------------------
+
+;(Y:index into in-battle inventory)
+;(Returns A: bitmask depending on equipment type and some character properties)
+; format seems to be 2 bits per character, 00 for usable and 10 for not
+.proc GetItemUsableY
+.reloc ; TODO: don't know why this is needed
+
+_0369:
+        lda Temp,Y
+        bpl :+
+        lda #$AA		;usable for none
+        jmp Ret
+:	and #$40		;Consumable
+        beq Equipment
+        lda InventoryFlags,Y
+        and #$20
+        beq RetZero
+        lda #$AA		;usable for none
+        bra Ret
+RetZero:								
+        lda #$00		;usable for all
+        bra Ret
+Equipment:		;**optimize: this whole section is basically a copy of the GetItemUsableA $455E subroutine		
+        lda Temp,Y
+        asl
+        asl
+        tax
+        tdc
+        tay
+        									;:					
+:	lda !ROMItemEquippable,X
+        sta !TempEquippable,Y
+        inx
+        iny
+        cpy #$0004
+        bne :-
+        									;.					
+        tdc
+        tax
+        tay
+        lda #$AA
+        sta $0E
+        									;:					
+DetermineEquippableLoop:
+        lda CharEquippable::Weapons,X
+        and TempEquippable::Weapons,Y
+        bne Match
+        lda CharEquippable::Weapons+1,X
+        and TempEquippable::Weapons+1,Y
+        bne Match
+        lda CharEquippable::Armor,X
+        and TempEquippable::Armor,Y
+        bne Match
+        lda CharEquippable::Armor+1,X
+        and TempEquippable::Armor+1,Y
+        beq NextChar
+Match:
+        txa
+        lsr
+        lsr
+        bne Check1
+        									;.					
+        lda $0E
+        and #$7F		;clear first character bit
+        sta $0E
+        bra NextChar
+Check1:								
+        cmp #$01
+        bne Check2
+        lda $0E
+        and #$DF		;clear second character bit
+        sta $0E
+        bra NextChar
+Check2:								
+        cmp #$02
+        bne Other
+        lda $0E
+        and #$F7         	;clear third character bit
+        sta $0E
+        bra NextChar
+Other:								
+        lda $0E
+        and #$FD		;clear fourth character bit
+        sta $0E
+NextChar:
+        inx
+        inx
+        inx
+        inx
+        cpx #$0010			;4 bytes * 4 characters
+        bne DetermineEquippableLoop
+        lda $0E
+Ret:	rts
+
+.endproc
+
+; ---------------------------------------------------------------------------
+
+;(sets up equipment type, targetting, and usable bytes for one item in battle inventory)
+;(A: item#)
+;(Y: inventory offset to write data about item)
+;Returns with EquipmentType low bytes in Temp ($2620), $40 there if it's a consumable, $80 if unusable
+;TODO: find a better way to access into the D1 item data because I didnt know how the translation from asar to ca65 would work
+.proc SetupInventoryInfo
+
+_03FA:
+        beq ItemZero
+        cmp #$E0
+        bcs Consumable		;>$E0 is consumable
+        cmp #$80
+        bcc Weapon		;<$80 is a weapon
+Armor:				;otherwise it's armor
+        sec
+        sbc #$80		;remove the armor offset
+        longa
+        asl
+        asl
+        sta $0E
+        asl
+        clc
+        adc $0E			;armor *12 (size of equipment struct)
+        tax
+        shorta0
+        lda ROMArmor,X
+        and #$08		;target enemy?
+        beq ItemZero
+        lda ROMArmor+2,X
+        and #$3F		;mask to equip info
+        sta Temp,Y
+        lda #$5A
+        sta InventoryFlags,Y
+        bra Ret
+ItemZero:	;or armor targettng bit 08h
+        lda #$80		;not usable
+        sta Temp,Y
+        lda #$5A
+        sta InventoryFlags,Y
+        bra Ret
+Weapon:
+        longa
+        asl
+        asl
+        sta $0E
+        asl
+        clc
+        adc $0E         	;weapon *12
+        tax
+        shorta0
+        lda ROMWeapons+4,X
+        and #$80
+        jsr ShiftDivide::_32	;shift to 04h bit
+        sta InventoryFlags,Y
+        lda ROMWeapons,X
+        sta InventoryTargetting,Y
+        lda ROMWeapons+2,X
+        pha
+        and #$C0		;flag bits from equipment type(? and throwable)
+        ora #$1A		;set some more bits (??)
+        ora InventoryFlags,Y	;keep existing bits (double grip)
+        sta InventoryFlags,Y
+        pla
+        and #$3F		;mask to just equip info
+        sta Temp,Y
+        bra Ret
+Consumable:								
+        sec
+        sbc #$E0
+        longa
+        asl
+        asl
+        asl
+        tax
+        shorta0
+        lda ROMConsumables::Targetting,X
+        sta InventoryTargetting,Y
+        lda ROMConsumables::Misc,X
+        sta InventoryFlags,Y
+        lda #$40		;consumable
+        sta Temp,Y
+Ret: 	RTS
+
+.endproc
+
+; ---------------------------------------------------------------------------
+
+;Utility routine used for the +% HP/MP passives on level up
+;inputs: 
+;$2E: 4 byte multiply result (value * percentage)
+;$08: 2 byte base value
+;$0A: 2 byte cap (generally 999 or 9999)
+;output:
+;$08: 2 byte value (value*percentage/100)+Base, capped
+.proc ApplyPercentage
+
+_0491:
+        ldx #$000F
+:	stz $0E,X		;clear $0E-1D
+        dex
+        bpl :-
+        ldx #$0064		;100
+        stx $12
+        ldx $2E			;previous multiply result (low bytes)
+        stx $0E
+        lda $30			;(high bytes)
+        sta $10
+;32 bit division routine
+;Dividend: 	$0E-11
+;Divisor: 	$12-15
+;Quotient: 	$16-19
+;Remainder: 	$1A-1C
+;**optimize: make this a general purpose subroutine, duplicate at $57FE
+        longa
+        clc
+        ldx #$0020
+:	rol $0E
+        rol $10
+        rol $1A
+        rol $1C
+        sec
+        lda $1A
+        sbc $12
+        sta $1A
+        lda $1C
+        sbc $14
+        sta $1C
+        bcs :+
+        lda $1A
+        adc $12
+        sta $1A
+        lda $1C
+        adc $14
+        sta $1C
+        clc
+:	rol $16
+        rol $18
+        dex
+        bne :--
+        shorta0
+;division ends here
+        clc
+        lda $16		;quotient, input/100
+        adc $08		;base value
+        sta $08		;adjusted value
+        lda $17		;high byte of above
+        adc $09
+        sta $09
+        sec 		;checks against 9999
+        lda $08
+        sbc $0A		;9999 low byte
+        lda $09
+        sbc $0B		;9999 high byte
+        bcc Ret
+        lda $0A		;caps at 9999
+        sta $08
+        lda $0B
+        sta $09
+Ret:	rts
+
+.endproc
+
+; ---------------------------------------------------------------------------
